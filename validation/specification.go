fe8d470cfae1eac04f6991c29a7980a43474f8bf
jacexh <jace.xh@gmail.com>
2022-01-25 07:02:55 +0000

    补充all\any逻辑

--- validation/specification.go
+++ validation/specification.go
@@ -15,3 +15,25 @@ func Or(s1, s2 Specification) Specification {
 		return s1(i) || s2(i)
 	}
 }
+
+func All(ss ...Specification) Specification {
+	return func(i interface{}) bool {
+		for _, spec := range ss {
+			if !spec(i) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+func Any(ss ...Specification) Specification {
+	return func(i interface{}) bool {
+		for _, spec := range ss {
+			if spec(i) {
+				return true
+			}
+		}
+		return false
+	}
+}
