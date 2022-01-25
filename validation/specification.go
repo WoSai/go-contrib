f8a2c2c072b298e237f42631b9e48db1c379a26f
jacexh <jace.xh@gmail.com>
2022-01-25 09:50:13 +0000

    重新定义Specification

--- validation/specification.go
+++ validation/specification.go
@@ -1,39 +1,160 @@
 package validation
 
-type (
-	Specification func(interface{}) bool
+import (
+	"fmt"
+	"strings"
 )
 
-func And(s1, s2 Specification) Specification {
-	return func(i interface{}) bool {
-		return s1(i) && s2(i)
+type (
+	// Specification define a interface
+	Specification interface {
+		// IsSatisfiedBy check if candidate matched the specification
+		IsSatisfiedBy(candidate interface{}) bool
+		// Rule print the specification details
+		Rule() string
 	}
+
+	AndCondition struct {
+		left  Specification
+		right Specification
+	}
+
+	AndNotCondition struct {
+		left  Specification
+		right Specification
+	}
+
+	OrCondition struct {
+		left  Specification
+		right Specification
+	}
+
+	NotCondition struct {
+		spec Specification
+	}
+
+	OrNotCondition struct {
+		left  Specification
+		right Specification
+	}
+
+	AllCondition struct {
+		specs []Specification
+	}
+
+	AnyCondition struct {
+		specs []Specification
+	}
+)
+
+var (
+	_ Specification = (*AndCondition)(nil)
+	_ Specification = (*OrCondition)(nil)
+	_ Specification = (*AndNotCondition)(nil)
+	_ Specification = (*OrNotCondition)(nil)
+	_ Specification = (*NotCondition)(nil)
+	_ Specification = (*AllCondition)(nil)
+	_ Specification = (*AndCondition)(nil)
+)
+
+func NewAndSpecification(s1, s2 Specification) Specification {
+	return &AndCondition{s1, s2}
 }
 
-func Or(s1, s2 Specification) Specification {
-	return func(i interface{}) bool {
-		return s1(i) || s2(i)
-	}
+func (and *AndCondition) IsSatisfiedBy(candidate interface{}) bool {
+	return and.left.IsSatisfiedBy(candidate) && and.right.IsSatisfiedBy(candidate)
 }
 
-func All(ss ...Specification) Specification {
-	return func(i interface{}) bool {
-		for _, spec := range ss {
-			if !spec(i) {
-				return false
-			}
+func (and *AndCondition) Rule() string {
+	return fmt.Sprintf("(%s) AND (%s)", and.left.Rule(), and.right.Rule())
+}
+
+func NewOrCondition(s1, s2 Specification) Specification {
+	return &OrCondition{s1, s2}
+}
+
+func (or *OrCondition) IsSatisfiedBy(candidate interface{}) bool {
+	return or.left.IsSatisfiedBy(candidate) || or.right.IsSatisfiedBy(candidate)
+}
+
+func (or *OrCondition) Rule() string {
+	return fmt.Sprintf("(%s) OR (%s)", or.left.Rule(), or.right.Rule())
+}
+
+func NewAndNotCondition(s1, s2 Specification) Specification {
+	return &AndNotCondition{s1, s2}
+}
+
+func (an *AndNotCondition) IsSatisfiedBy(candidate interface{}) bool {
+	return an.left.IsSatisfiedBy(candidate) && (!an.right.IsSatisfiedBy(candidate))
+}
+
+func (an *AndNotCondition) Rule() string {
+	return fmt.Sprintf("(%s) AND !(%s)", an.left.Rule(), an.right.Rule())
+}
+
+func NewNotCondition(spec Specification) Specification {
+	return &NotCondition{spec: spec}
+}
+
+func (not *NotCondition) IsSatisfiedBy(candidate interface{}) bool {
+	return !not.spec.IsSatisfiedBy(candidate)
+}
+
+func (not *NotCondition) Rule() string {
+	return fmt.Sprintf("!(%s)", not.spec.Rule())
+}
+
+func NewOrNotCondition(s1, s2 Specification) Specification {
+	return &OrNotCondition{s1, s2}
+}
+
+func (on *OrNotCondition) IsSatisfiedBy(candidate interface{}) bool {
+	return on.left.IsSatisfiedBy(candidate) || (!on.right.IsSatisfiedBy(candidate))
+}
+
+func (on *OrNotCondition) Rule() string {
+	return fmt.Sprintf("(%s) OR !(%s)", on.left.Rule(), on.right.Rule())
+}
+
+func NewAllCondition(specs ...Specification) Specification {
+	return &AllCondition{specs: specs}
+}
+
+func (all *AllCondition) IsSatisfiedBy(candidate interface{}) bool {
+	for _, spec := range all.specs {
+		if !spec.IsSatisfiedBy(candidate) {
+			return false
 		}
-		return true
 	}
+	return true
 }
 
-func Any(ss ...Specification) Specification {
-	return func(i interface{}) bool {
-		for _, spec := range ss {
-			if spec(i) {
-				return true
-			}
-		}
-		return false
+func (all *AllCondition) Rule() string {
+	rules := make([]string, len(all.specs))
+	for i := 0; i < len(all.specs); i++ {
+		rules[i] = all.specs[i].Rule()
 	}
+	return fmt.Sprintf("ALL[%s]", strings.Join(rules, ", "))
+}
+
+func NewAnyCondition(specs ...Specification) Specification {
+	return &AnyCondition{specs: specs}
+}
+
+func (any *AnyCondition) IsSatisfiedBy(candidate interface{}) bool {
+	for _, spec := range any.specs {
+		if spec.IsSatisfiedBy(candidate) {
+			return true
+		}
+	}
+	return false
+}
+
+func (any *AnyCondition) Rule() string {
+	rules := make([]string, len(any.specs))
+	for i := 0; i < len(any.specs); i++ {
+		rules[i] = any.specs[i].Rule()
+	}
+	return fmt.Sprintf("ANY[%s]", strings.Join(rules, ", "))
 }
--- validation/specification_test.go
+++ validation/specification_test.go
@@ -1,37 +1,69 @@
 package validation
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func isHuman(v interface{}) bool {
-	return true
-}
+type testSpecV1 struct{}
+type testSpecV2 struct{}
 
-func isBoy(v interface{}) bool {
-	return true
-}
-
-func isJimmy(v interface{}) bool {
+func (ts *testSpecV1) IsSatisfiedBy(candidate interface{}) bool {
+	v, ok := candidate.(int)
+	if ok {
+		return v >= 10
+	}
 	return false
 }
 
-func TestSpecification(t *testing.T) {
-	ret := And(isHuman, isBoy)(struct{}{})
-	if !ret {
+func (ts *testSpecV1) Rule() string {
+	return "value >= 10"
+}
+
+func (ts2 *testSpecV2) IsSatisfiedBy(candidate interface{}) bool {
+	v, ok := candidate.(int)
+	if ok {
+		return v < 80
+	}
+	return false
+}
+
+func (ts2 *testSpecV2) Rule() string {
+	return "value < 80"
+}
+
+func TestSpec(t *testing.T) {
+	var s1 = new(testSpecV1)
+	var s2 = new(testSpecV1)
+	var s3 = new(testSpecV2)
+
+	a1 := NewAndSpecification(s1, s2)
+	fmt.Println(a1.Rule())
+	if !a1.IsSatisfiedBy(20) {
+		t.FailNow()
+	}
+	if a1.IsSatisfiedBy(5) {
 		t.FailNow()
 	}
 
-	ret = Or(isBoy, isJimmy)(struct{}{})
-	if !ret {
+	a2 := NewAndSpecification(a1, s3)
+	fmt.Println(a2.Rule())
+	if !a2.IsSatisfiedBy(50) {
 		t.FailNow()
 	}
 
-	ret = All(isHuman, isBoy, isJimmy)(struct{}{})
-	if ret {
+	a3 := NewAndNotCondition(a1, s3)
+	fmt.Println(a3.Rule())
+	if !a3.IsSatisfiedBy(90) {
+		t.FailNow()
+	}
+	if a3.IsSatisfiedBy(70) {
 		t.FailNow()
 	}
 
-	ret = Any(isHuman, isBoy, isJimmy)(struct{}{})
-	if !ret {
+	a4 := NewAnyCondition(s1, a3)
+	fmt.Println(a4.Rule())
+	if !a4.IsSatisfiedBy(40) {
 		t.FailNow()
 	}
 }
