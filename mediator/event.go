7c49737039f5cf55dfdbe5c847cd19f3509a9a29
jacexh <jace.xh@gmail.com>
2022-01-26 01:32:23 +0000

    update

--- mediator/event.go
+++ mediator/event.go
@@ -17,23 +17,28 @@ type (
 		Type() EventType
 	}
 
-	EventCollection struct {
+	eventCollection struct {
 		events []Event
 		raised int32
 	}
+
+	EventCollection interface {
+		Add(Event)
+		Raise(context.Context, Mediator)
+	}
 )
 
-func NewEventCollection() *EventCollection {
-	return &EventCollection{events: make([]Event, 0)}
+func NewEventCollection() EventCollection {
+	return &eventCollection{events: make([]Event, 0)}
 }
 
-func (es *EventCollection) Add(ev Event) {
+func (es *eventCollection) Add(ev Event) {
 	if atomic.LoadInt32(&es.raised) == 0 {
 		es.events = append(es.events, ev)
 	}
 }
 
-func (es *EventCollection) Raise(ctx context.Context, m Mediator) {
+func (es *eventCollection) Raise(ctx context.Context, m Mediator) {
 	if atomic.CompareAndSwapInt32(&es.raised, 0, 1) {
 		for _, event := range es.events {
 			m.Dispatch(ctx, event)
